2da12f58418a0faa994037539b005bcc5566e7ec
JCARL0 <john.magno@estudiant.fjaverianas.com>
2024-05-19 22:37:54 +0200

    PROJ3

--- countries.py
+++ countries.py
@@ -2,63 +2,75 @@ import pandas as pd
 import random
 from flask import Flask, render_template, request
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__)
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv('./proj3/static/countries/countries.csv', delimiter=';', encoding='latin1')  # Specify the delimiter and encoding
+# Reads the CSV file 
+df = pd.read_csv('./proj3/static/countries/countries.csv')
 
-# Initialize wrong guess counter and country_name variable
-wrong_guesses = 0
-country_name = ""
-random_row = None  # Define random_row globally
-
+#Function to reset game
 def reset_game():
-    global country_name, wrong_guesses, random_row
-    # Get a random row from the DataFrame
-    random_row = df.sample()
-    # Get the country name from the random row
-    country_name = random_row['Country'].values[0]
-    # Reset wrong guess counter
-    wrong_guesses = 0
+    global country_name, wrong_guesses, random_row, message #declares the variables as global variables
+    random_row = df.sample() #selects a random (country) row from the CSV 
+    country_name = random_row['Country'].values[0] #extracts the value of the 'Country' column then assigns it to 'country_name'
+    wrong_guesses = 0 #Counts the number of wrong guesses (0=default)
+    message = ""  #Message("---empty---"=default)
 
-# Pass the random column name, country data, and filenames to your template
+#Route for the main URL
 @app.route('/')
 def index():
-    global wrong_guesses, random_row, country_name
-    reset_game()  # Reset the game for a new round
-    random_column_name = random.choice(df.columns)
-    return render_template('index.html', country=random_row.to_dict(), country_name=country_name, random_column_name=random_column_name, wrong_guesses=wrong_guesses)
+    reset_game() #Calls for the reset function
+    random_column_name = random.choice(df.columns) #Selects a random column name from the DataFrame
+    return render_template('index.html', country=random_row.to_dict(), #Renders the index.html
+                           country_name=country_name, 
+                           random_column_name=random_column_name, 
+                           wrong_guesses=0)
 
+#Route for the results URL
 @app.route('/results', methods=['POST'])
 def results():
-    global wrong_guesses, country_name, random_row
-    guess = request.form.get('guess')
-    action = request.form.get('action')
+    guess = request.form.get('guess') #Retrieves the value of the form field named 'guess' from the request form
+    action = request.form.get('action') #Retrieves the value of the form field named 'action' from the request form
 
-    if action == 'skip':
-        reset_game()  # Reset the game for a new round
+    if action in ['skip', 'play_again']: #checks if the value of 'action' is either 'skip' or 'play_again'
+        reset_game() #If it is one of the two, it resets the game
         random_column_name = random.choice(df.columns)
-        return render_template('index.html', country=random_row.to_dict(), country_name=country_name, random_column_name=random_column_name, wrong_guesses=wrong_guesses)
+        return render_template('index.html', country=random_row.to_dict(), 
+                               country_name=country_name, 
+                               random_column_name=random_column_name, 
+                               wrong_guesses=0)
 
-    if guess.lower() != country_name.lower():
-        wrong_guesses += 1  # Increment wrong guess counter
-        message = "Incorrect guess. Please try again."
-        
-        # Determine what information to reveal based on wrong guess count
-        reveal_info = ""
-        if wrong_guesses == 2:
+    global message #Declares the variable 'message' as global
+    if guess.lower() != country_name.lower(): #checks if the lowercase version of the user's guess does not match the lowercase version of the actual country name
+        wrong_guesses = int(request.form.get('wrong_guesses')) + 1 #retrieves the value of 'wrong_guesses' then 1 is added in the 'wrong_guesses'
+        message = "Incorrect guess. Please try again." #Message if answer in incorrect
+        reveal_info = "" #Initializes the variable 
+        if wrong_guesses == 2: #Reveals the continents where the country is from
             reveal_info = "Continent: " + random_row['Continent'].values[0]
-        elif wrong_guesses == 4:
+        elif wrong_guesses == 4: #Reveals the capital of the country
             reveal_info = "Capital: " + random_row['Capital'].values[0]
-        elif wrong_guesses == 6:
+        elif wrong_guesses == 6:#Reveals the flag of the country
             reveal_info = "Flag: " + random_row['Flag'].values[0]
-        elif wrong_guesses >= 8:  # If more than 6 wrong guesses, show the correct answer
+        elif wrong_guesses >= 8: #When 'wrong_guesses' reaches 8, the game is over
             message = "You've run out of guesses. The correct answer was " + country_name
-        return render_template('index.html', country=random_row.to_dict(), country_name=country_name, message=message, reveal_info=reveal_info, wrong_guesses=wrong_guesses)
+        remaining_guesses = 8 - wrong_guesses  # Calculates remaining guesses
+        return render_template('index.html', country=random_row.to_dict(), 
+                               country_name=country_name, 
+                               message=message, 
+                               reveal_info=reveal_info, 
+                               wrong_guesses=wrong_guesses,
+                               remaining_guesses=remaining_guesses,
+                               correct_guess=False)  # Pass correct_guess as False for incorrect guess
     else:
-        reset_game()  # Reset the game for a new round
-        random_column_name = random.choice(df.columns)
-        return render_template('index.html', country=random_row.to_dict(), country_name=country_name, message="You've guessed the correct answer!", random_column_name=random_column_name, wrong_guesses=wrong_guesses)
+        reset_game() #Resets game
+        random_column_name = random.choice(df.columns) #Selects a random column name from the DataFrame
+        return render_template('index.html', country=random_row.to_dict(), #Renders the index.html template
+                               country_name=country_name, 
+                               message="You've guessed the correct answer!", #Message indicating that the guess is wrong
+                               random_column_name=random_column_name, 
+                               wrong_guesses=0,
+                               remaining_guesses=8,
+                               correct_guess=True)  #Boolean value indicating that the guess was correct
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True) #Ensures that the Flask application defined in the script is run
+    
